a8bd6202954f844f6af48020e4a45c15cb8cfcbb
giseok <river.key93@gmail.com>
2021-03-08 21:22:02 +0900

    reverse를 이용한 문제해결 (list에서만 가능)

--- 6_2_reverse_string.py
+++ 6_2_reverse_string.py
@@ -1,10 +1,6 @@
 class Solution:
     def reverseString(self, s: list[str]) -> None:
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        s.reverse()
 
 
 a = Solution()
