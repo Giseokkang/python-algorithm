49792144c69609cf9b15c77268dcc272b8ab7095
giseok <river.key93@gmail.com>
2021-03-15 20:51:07 +0900

    most-common-word

--- 6_4_most_common_word.py
+++ 6_4_most_common_word.py
@@ -9,9 +9,6 @@ class Solution:
             for word in re.sub(r"[^\w]", " ", paragraph).lower().split()
             if word not in banned
         ]
-        counts = collections.defaultdict(int)
-        for word in words:
-            counts[word] += 1
         counts = collections.Counter(words)
         return counts.most_common(1)[0][0]
 
